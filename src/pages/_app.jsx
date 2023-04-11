e9f966d8dc279f978ae1f9d5890ab0d47b39c86a
rdx <31172722+rd-xx@users.noreply.github.com>
2023-04-11 11:09:28 +0200

    feat: remove default template

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,4 @@
-import "../style.css";
-import "../App.css";
+import '../styles/globals.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
